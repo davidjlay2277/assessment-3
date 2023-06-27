f161368834e2f62bb2364cff5f85c260f11936ca
David Lay <davidjlay2277@gmail.com>
2023-06-26 21:44:38 -0500

    building profile,js

--- profile.js
+++ profile.js
@@ -1,9 +1,27 @@
 
-const button2 = document.querySelector('place');
-const button3 = document.querySelector('ritual');
+console.log('hello world');
 
-form.addEventListener('click', faveColor);
-
-function faceColor() {
-    const button1 = document.querySelector('color');
+function faveColor(event) {
+  alert("My favorite color is #00008B");
 }
+
+const button1 = document.querySelector("#color");
+button1.addEventListener("click", faveColor);
+
+
+
+
+
+// let button2 = document.querySelector("#place");
+// function favePlace(event) {
+//   alert("My favorite place is DevMt");
+// }
+
+// let button3 = document.querySelector("#ritual");
+// function faveRitual(event) {
+//   alert("My favorite ritual is coding in my dreams");
+// }
+
+
+// button2.addEventListener("click", favePlace);
+// button3.addEventListener("click", faveRitual);
